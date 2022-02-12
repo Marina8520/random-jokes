336719f2325baad0eb20e960d3aabe188b823daf
Marina8520 <4079722@mail.ru>
2022-02-12 23:58:50 +0300

    feat: add main container

---
+++ index.js
@@ -0,0 +1,51 @@
+const container = document.querySelector('.container');
+const button = document.querySelector('.btn');
+const url = 'https://api.unsplash.com/photos/random?query=fun&client_id=Do42olqenMyCvWlNkjzCGeFDbssllTMl57rssopeupQ'
+const url2 = 'https://type.fit/api/quotes';
+const p = document.createElement('p');
+const img = document.createElement('img');
+const body = document.querySelector('body');
+
+ 
+function showImg(data)  {
+img.classList.add('gallery-img')
+img.src = data.urls.regular;
+img.alt = `image`; 
+container.append(img);
+img.style.width = "300px";   
+}
+ 
+/*
+function showImg(data)  {
+    container.style.backgroundColor = 'white';
+  //  document.body.style.backgroundImage = "url('data.urls.regular')";
+
+}
+ */
+
+async function getImg() {
+    const res = await fetch(url);
+    const data1 = await res.json();
+    console.log(data1);
+    showImg(data1);
+}
+getImg();
+
+function showData(data2) {
+    p.textContent = data2[Math.floor(Math.random()*data2.length)].text;
+    p.style.fontSize = '24px';
+    p.style.color = "#9f918e";
+    p.style.width = "100% - 300px"
+    container.append(p);
+};
+
+async function getData() {
+    const res = await fetch(url2);
+    const data2 = await res.json();
+    console.log(data2); 
+    showData(data2);
+  }
+  getData();
+
+  button.addEventListener('click', getData);
+  button.addEventListener('click', getImg);
